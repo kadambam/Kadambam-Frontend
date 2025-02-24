9f94f761e28502da326358d5acd662d77b1e289c
sanjaynishanth <sanjaynishanth.ss@gmail.com>
2025-02-24 18:46:56 +0530

    page6update

--- src/components/Page-6/Page6.jsx
+++ src/components/Page-6/Page6.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
+
 import Navbar from '../Page-1/Navbar'
 import Hero from './Hero'
-
-const Page6 = () => {
-    return (
-      <div> 
-        <Navbar/>
-        <Hero/>
-      </div>
-    )
-  }
-  
-  export default Page6
-=======
 
 const Page6 = () => {
   return (
@@ -24,4 +12,3 @@ const Page6 = () => {
 }
 
 export default Page6
->>>>>>> 47d83d3f31301f98eef33f5e8346afbdad44522c
